e437365c95685976bbc9b68bb3c50cddfd837d22
William Daigle <daiglewilliam13@gmail.com>
2024-09-24 09:19:34 -0700

    fix styling

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import {featuredGuests} from '/images/data.js';
+import {featuredGuests} from './images/data.js';
 
 let featuredSection = document.getElementById('featured');
 
@@ -13,14 +13,26 @@ featuredGuests.map((guestObj)=>{
         <img class="images" width="600px" src="${url}" alt="">
         `;
     })
+    let nameList = '';
+    guestObj.guestNames.forEach((name, index, array)=>{
+        let next = array[index+1];
+        if(next){
+            nameList += `${name} & `;
+        } else {
+            nameList += `${name}`;
+        }
+        
+    })
     let htmlToInject;
     console.log('imghtml: ', imgHTML)
     htmlToInject = `
     <div class="feat-wrapper">
         <div class="info-wrapper">
             <div class='bg-filter'></div>
-           <h2>Panel: ${guestObj.panelName}</h2>
+           <h2>${guestObj.panelName}</h2>
            <p>${guestObj.panelDesc}</p>
+           <p>Featuring: ${nameList}</p>
+           <p><a class="cta-button" href="">Check back soon for details!</a>
         </div>
            ${imgHTML}
     </div>
